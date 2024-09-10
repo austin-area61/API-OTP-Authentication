b3fc991601f7dda9923cbe3c9be90b3b683dd40f
austin-area61 <austinonyango.area61@gmail.com>
2024-09-10 09:23:44 +0300

    configure smtp transporter.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,3 +3,13 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const nodemailer = require('nodemailer');
 
+//configure smtp
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
